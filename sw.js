73dc9c1419db1eb1622d6cac5a5e9e849bb098c3
GeOnE <emedarias@gmail.com>
2019-12-03 16:12:14 -0300

    Instalar y activar

--- sw.js
+++ sw.js
@@ -3,3 +3,6 @@ self.addEventListener('install', evt =>{console.log("Service Worker Registrado."
 
 //Activar el service worker.
 self.addEventListener('activate', evt =>{ console.log("Service Worker Activado." );});
+
+//Eventos Fetch (fetch request o pedido de busqueda).
+self.addEventListener('fetch', evt =>{console.log("Evento fetch", evt);});
