eb32da7d80651ab7ac3fbd5ed2ef7d41987f99c6
GeOnE <emedarias@gmail.com>
2019-12-04 12:08:46 -0300

    Evento fetch

--- sw.js
+++ sw.js
@@ -3,7 +3,8 @@ const elementos=["index.html","css/estilo.css","js/accion.js"];
 
 //Instalar el service worker.
 self.addEventListener("install", evt => {
-	//console.log("El service worker se instalo.");
+	console.log("El service worker se instalo.");
+	//1°-Guardamos en el cache, los elementos predefinidos de la pagina. Css, Javascripts y HTML.
 	evt.waitUntil(caches.open(nombreCache).then((cache) => {
 					console.log("Definimos el cache predeterminado.");
 					cache.addAll(elementos);
@@ -18,4 +19,12 @@ self.addEventListener("activate", evt =>{
 	});
 
 //Eventos Fetch (fetch request o pedido de busqueda).
-self.addEventListener("fetch", evt =>{console.log("Se atrapo el evento: ",evt);});
+self.addEventListener("fetch", evt =>{
+	console.log("Se atrapo el evento: ",evt);
+	//2°-Atrapamos los pedidos, para que los busque en el cache.
+	evt.respondWith(
+		caches.match(evt.request).then(cacheRes => {
+			return cacheRes || fetch(evt.request)
+		})
+	);
+});
