8a14f30d2bbeda1d292cc03df84df6b7fe3db85e
GeOnE <emedarias@gmail.com>
2019-12-04 11:39:22 -0300

    sw

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const nombreStatic="sitio-cache";
-const elementos=["/","/index.html","/css/estilo.css","/js/accion.js"];
+const elementos=["index.html","css/estilo.css","js/accion.js"];
 
 //Instalar el service worker.
 /*self.addEventListener("install", evt => {
