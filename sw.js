20ab1ec4114b160eb01c82d00042e6ea546fb577
GeOnE <emedarias@gmail.com>
2019-12-04 12:15:18 -0300

    Evento fetch

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const nombreCache="sitio-cache";
-const elementos=["index.html","css/estilo.css","js/accion.js"];
+const elementos=["","index.html","css/estilo.css","js/accion.js"];
 
 //Instalar el service worker.
 self.addEventListener("install", evt => {
