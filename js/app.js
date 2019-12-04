7d2e92a5237216d9f79e29b373818038a5ab7ec1
GeOnE <emedarias@gmail.com>
2019-12-04 14:14:59 -0300

    arreglado el manifest

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 if('serviceWorker' in navigator)
 	{
 		console.log("Acepta service worker");
-		navigator.serviceWorker.register('sw.js',{scope:'/Ta-Te-Ti-PWA/'})
+		navigator.serviceWorker.register('sw.js')
 		.then((reg) => console.log("Service Worker Registrado",reg)) /*Si el registro sucede, entoces realiza esto.*/
 		.catch((err) => console.log("Service Worker No Registrado",err)); /*Sino, atrapa el error e informa.*/
 	}
