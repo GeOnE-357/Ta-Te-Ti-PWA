02288555bb87d76cf2116fd2aa5f4e930a77f192
GeOnE <emedarias@gmail.com>
2019-12-04 11:22:31 -0300

    sw

--- sw.js
+++ sw.js
@@ -2,13 +2,23 @@ const nombreCache="sitio-cache";
 const elementos=["/","/index.html","/css/estilo.css","/js/accion.js"];
 
 //Instalar el service worker.
-self.addEventListener("install", evt => {
+/*self.addEventListener("install", evt => {
 	//console.log("El service worker se instalo.");
 	evt.waitUntil(caches.open(nombreCache).then((cache) => {
 					console.log("Definimos el cache predeterminado.");
 					cache.addAll(elementos);
 				})
 		);
+});*/
+
+self.addEventListener('install', evt => {
+  //console.log('service worker installed');
+  evt.waitUntil(
+    caches.open(nombreCache).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(elementos);
+    })
+  );
 });
 
 //Activar el service worker.
