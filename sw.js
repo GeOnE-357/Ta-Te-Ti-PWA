38ab7c93b22c44a3849713b4c67991bf26d57e89
GeOnE <emedarias@gmail.com>
2019-12-05 22:24:22 -0300

    ver keys

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ self.addEventListener("install", evt => {
 	console.log("El service worker se instalo.");
 	//1°-Guardamos en el cache, los elementos predefinidos de la pagina. Css, Javascripts y HTML.
 	evt.waitUntil(caches.open(nombreCache).then((cache) => {
-					console.log("Definimos el cache predeterminado.");
+					//console.log("Definimos el cache predeterminado.");
 					cache.addAll(elementos);
 				})
 		);
@@ -15,17 +15,17 @@ self.addEventListener("install", evt => {
 
 //Activar el service worker.
 self.addEventListener("activate", evt =>{
-		console.log("El service worker se activo.");
+		//console.log("El service worker se activo.");
 		//3°-Chequearemos la version de cache, borraremos la version obsoleta.
-		/*evt.waitUntil(
+		evt.waitUntil(
 			caches.keys().then(keys => {
-				//console.log(keys);
-				return Promise.all(keys
-					.filter(key => key !== nombreCache)
-					.map(key => caches.delete(key))
-				)
+				console.log(keys);
+				//return Promise.all(keys
+				//	.filter(key => key !== nombreCache)
+				//	.map(key => caches.delete(key))
+				//)
 			})
-		);*/
+		);
 	});
 
 //Eventos Fetch (fetch request o pedido de busqueda).
